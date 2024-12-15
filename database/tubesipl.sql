cfb922a96c2567a20241d515f2b9353fbb4aef00
LAPTOP-VLNPEOGR\Fadlan <mfadlandzikra@gmail.com>
2024-12-16 06:30:37 +0700

    update db karyawan

--- database/tubesipl.sql
+++ database/tubesipl.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 15, 2024 at 09:22 AM
+-- Generation Time: Dec 16, 2024 at 12:28 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -48,20 +48,26 @@ INSERT INTO `admin` (`id`, `username`, `password`) VALUES
 
 CREATE TABLE `karyawan` (
   `id_karyawan` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `nama` varchar(255) NOT NULL,
-  `alamat` text NOT NULL,
-  `jabatan` varchar(50) NOT NULL,
-  `gaji` float NOT NULL
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `nama_lengkap` varchar(255) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `nomor_hp` varchar(15) DEFAULT NULL,
+  `alamat` varchar(255) DEFAULT NULL,
+  `jabatan` varchar(100) DEFAULT NULL,
+  `gaji_pokok` decimal(10,2) DEFAULT 0.00,
+  `jatah_cuti` int(11) DEFAULT 12,
+  `cuti_terpakai` int(11) DEFAULT 0,
+  `status_kehadiran` enum('Hadir','Tidak Hadir','Izin','Sakit') DEFAULT 'Hadir',
+  `tanggal_masuk` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `karyawan`
 --
 
-INSERT INTO `karyawan` (`id_karyawan`, `username`, `password`, `nama`, `alamat`, `jabatan`, `gaji`) VALUES
-(1, 'user', 'ee11cbb19052e40b07aac0ca060c23ee', 'nama karyawan', 'alamat', 'karyawan tetap', 1000000);
+INSERT INTO `karyawan` (`id_karyawan`, `username`, `password`, `nama_lengkap`, `email`, `nomor_hp`, `alamat`, `jabatan`, `gaji_pokok`, `jatah_cuti`, `cuti_terpakai`, `status_kehadiran`, `tanggal_masuk`) VALUES
+(1, 'user', 'ee11cbb19052e40b07aac0ca060c23ee', 'nama lengkap', 'email@gmail.com', '081111111111', 'alamat', 'karyawan tetap', 0.00, 12, 0, 'Hadir', '2024-12-16');
 
 --
 -- Indexes for dumped tables
@@ -88,6 +94,12 @@ ALTER TABLE `karyawan`
 --
 ALTER TABLE `admin`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `karyawan`
+--
+ALTER TABLE `karyawan`
+  MODIFY `id_karyawan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
