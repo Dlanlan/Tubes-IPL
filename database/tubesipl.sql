931f3115c8107e40319f6fce8707dbca59588d98
SyaskiaA <saskiakia529@gmail.com>
2024-12-17 15:57:18 +0700

    menambah data karyawan

--- database/tubesipl.sql
+++ database/tubesipl.sql
@@ -3,13 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
-<<<<<<< HEAD
--- Generation Time: Dec 16, 2024 at 03:03 PM
-=======
--- Generation Time: Dec 11, 2024 at 01:38 AM
->>>>>>> parent of 5bd6cf0 (update database karyawan)
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Generation Time: Dec 17, 2024 at 09:39 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -22,84 +18,217 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: tubesipl
+-- Database: `tubesipl`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table admin
+-- Table structure for table `absen`
 --
 
-CREATE TABLE admin (
-  id int(11) NOT NULL,
-  username varchar(255) NOT NULL,
-  password varchar(255) NOT NULL
+CREATE TABLE `absen` (
+  `id_absensi` int(11) NOT NULL,
+  `id_karyawan` int(11) NOT NULL,
+  `tanggal` date NOT NULL,
+  `status` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table admin
+-- Dumping data for table `admin`
 --
 
-INSERT INTO admin (id, username, password) VALUES
+INSERT INTO `admin` (`id`, `username`, `password`) VALUES
 (1, 'admin', '21232f297a57a5a743894a0e4a801fc3');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table karyawan
+-- Table structure for table `cuti`
 --
 
-CREATE TABLE karyawan (
-  id_karyawan int(11) NOT NULL,
-  username varchar(50) NOT NULL,
-  password varchar(50) NOT NULL,
-  nama varchar(255) NOT NULL,
-  tmp_tgl_lahir varchar(255) NOT NULL,
-  jenkel varchar(255) NOT NULL,
-  agama varchar(255) NOT NULL,
-  alamat text NOT NULL,
-  no_tel varchar(18) NOT NULL
+CREATE TABLE `cuti` (
+  `id_cuti` int(11) NOT NULL,
+  `id_karyawan` int(11) NOT NULL,
+  `alasan` text NOT NULL,
+  `tanggal_mulai` date NOT NULL,
+  `tanggal_selesai` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table karyawan
+-- Dumping data for table `cuti`
 --
 
-INSERT INTO karyawan (id_karyawan, username, password, nama, tmp_tgl_lahir, jenkel, agama, alamat, no_tel) VALUES
+INSERT INTO `cuti` (`id_cuti`, `id_karyawan`, `alasan`, `tanggal_mulai`, `tanggal_selesai`) VALUES
+(2050810246, 1, 'izin menemani keluarga sakit', '2024-12-17', '2024-12-20');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gaji`
+--
+
+CREATE TABLE `gaji` (
+  `id_gaji` int(11) NOT NULL,
+  `id_karyawan` int(11) NOT NULL,
+  `jumlah_gaji` float NOT NULL,
+  `periode` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `karyawan`
+--
+
+CREATE TABLE `karyawan` (
+  `id_karyawan` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `tmp_tgl_lahir` varchar(255) NOT NULL,
+  `jenkel` varchar(255) NOT NULL,
+  `agama` varchar(255) NOT NULL,
+  `alamat` text NOT NULL,
+  `no_tel` varchar(18) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `karyawan`
+--
+
+INSERT INTO `karyawan` (`id_karyawan`, `username`, `password`, `nama`, `tmp_tgl_lahir`, `jenkel`, `agama`, `alamat`, `no_tel`) VALUES
 (1, 'user', 'ee11cbb19052e40b07aac0ca060c23ee', 'nama karyawan', 'tempat tanggal lahir', 'jenis kelamin', 'agama', 'alamat', 'nomor telepon');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `manajer`
+--
+
+CREATE TABLE `manajer` (
+  `id_manajer` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `id_cuti` int(11) NOT NULL,
+  `id_karyawan` int(11) NOT NULL,
+  `konfirmasi_cuti` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `manajer`
+--
+
+INSERT INTO `manajer` (`id_manajer`, `username`, `password`, `id_cuti`, `id_karyawan`, `konfirmasi_cuti`) VALUES
+(2050810501, 'elridho', '8c28a834f46d47ed613137ad4962b47d', 2050810246, 1, 1);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table admin
+-- Indexes for table `absen`
 --
-ALTER TABLE admin
-  ADD PRIMARY KEY (id);
+ALTER TABLE `absen`
+  ADD PRIMARY KEY (`id_absensi`),
+  ADD KEY `id_karyawan` (`id_karyawan`);
 
 --
--- Indexes for table karyawan
+-- Indexes for table `admin`
 --
-ALTER TABLE karyawan
-  ADD PRIMARY KEY (id_karyawan);
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `cuti`
+--
+ALTER TABLE `cuti`
+  ADD PRIMARY KEY (`id_cuti`),
+  ADD KEY `id_karyawan` (`id_karyawan`);
+
+--
+-- Indexes for table `gaji`
+--
+ALTER TABLE `gaji`
+  ADD PRIMARY KEY (`id_gaji`),
+  ADD KEY `id_karyawan` (`id_karyawan`);
+
+--
+-- Indexes for table `karyawan`
+--
+ALTER TABLE `karyawan`
+  ADD PRIMARY KEY (`id_karyawan`);
+
+--
+-- Indexes for table `manajer`
+--
+ALTER TABLE `manajer`
+  ADD PRIMARY KEY (`id_manajer`),
+  ADD KEY `id_cuti` (`id_cuti`),
+  ADD KEY `id_karyawan` (`id_karyawan`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table admin
+-- AUTO_INCREMENT for table `admin`
 --
-ALTER TABLE admin
-  MODIFY id int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT for table karyawan
+-- AUTO_INCREMENT for table `karyawan`
 --
-ALTER TABLE karyawan
-  MODIFY id_karyawan int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+ALTER TABLE `karyawan`
+  MODIFY `id_karyawan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `manajer`
+--
+ALTER TABLE `manajer`
+  MODIFY `id_manajer` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2050810502;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `absen`
+--
+ALTER TABLE `absen`
+  ADD CONSTRAINT `absen_ibfk_1` FOREIGN KEY (`id_karyawan`) REFERENCES `karyawan` (`id_karyawan`);
+
+--
+-- Constraints for table `cuti`
+--
+ALTER TABLE `cuti`
+  ADD CONSTRAINT `cuti_ibfk_1` FOREIGN KEY (`id_karyawan`) REFERENCES `karyawan` (`id_karyawan`);
+
+--
+-- Constraints for table `gaji`
+--
+ALTER TABLE `gaji`
+  ADD CONSTRAINT `gaji_ibfk_1` FOREIGN KEY (`id_karyawan`) REFERENCES `karyawan` (`id_karyawan`);
+
+--
+-- Constraints for table `manajer`
+--
+ALTER TABLE `manajer`
+  ADD CONSTRAINT `manajer_ibfk_1` FOREIGN KEY (`id_cuti`) REFERENCES `cuti` (`id_cuti`),
+  ADD CONSTRAINT `manajer_ibfk_2` FOREIGN KEY (`id_karyawan`) REFERENCES `karyawan` (`id_karyawan`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
